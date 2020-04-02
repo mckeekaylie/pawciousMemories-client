ee225aeba4d3a5ad85d0a8d3f6f19702efd97044
mckeekaylie <mckeekaylie@gmail.com>
2020-04-02 16:02:59 -0400

    start of auth

--- src/components/Auth/Auth.tsx
+++ src/components/Auth/Auth.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import Signup from '../Auth/Signup';
+import Signin from '../Auth/Signin';
 
 class Auth extends React.Component {
     render(){
-        return(
+        return (
+        <div>
+            <h1 id='appTitle'>My Concert Log</h1>
             <div>
-                
+                <div className='authRow'>
+                    <div className='signup'> 
+                        <Signup updateToken={this.props.updateToken} />
+                    </div>
+                    <div className='signin'>
+                        <Signin updateToken={this.props.updateToken}/>
+                    </div>
+                </div>
             </div>
+        </div> 
         )
     }
 }
--- src/components/Auth/Signin.tsx
+++ src/components/Auth/Signin.tsx
@@ -1,11 +1,57 @@
 import React from 'react';
 
-class Signin extends React.Component {
+// PROPS TYPE ALIAS
+type AcceptedProps = {
+    updateToken: string
+};
+  
+// STATE TYPE ALIAS
+type SigninState = {
+    email: '',
+    password: ''
+};
+
+class Signin extends React.Component<AcceptedProps, SigninState>{
+    constructor(props: AcceptedProps){
+        super(props);
+        this.state = {
+          email: '',
+          password: ''
+        }
+      }
+
+    // HANDLE SUBMIT  
+    handleSubmit = (event: React.FormEvent<HTMLInputElement>) => {
+        event.preventDefault();
+        fetch(`${APIURL}/user/signin`, {
+            method: 'POST',
+            body: JSON.stringify({email: this.state.email, password: this.state.password}),
+            headers: new Headers ({
+                'Content-Type': 'application/json'
+            })
+        }) .then(
+            (response) => response.json()
+        ) .then((data) => {
+            console.log(data);
+            this.props.updateToken = (data.sessionToken);
+        })
+
     render(){
         return(
-            <div>
-                
-            </div>
+            <div className='signin'>
+            <h1>Signin</h1>
+            <form onSubmit={this.handleSubmit}>
+                <div>
+                    <label htmlFor='email'>Email</label>
+                    <input placeholder='user@email.com' onChange = {(e) => setState(email: e.target.value)} name='email' value={this.state.email} />
+                </div>
+                <div>
+                    <label htmlFor='password'>Password</label>
+                    <input placeholder='Password' type='password' onChange={(e) => setState(password: e.target.value)} name='password' value={this.state.password} />
+                </div>
+                <button className='signinUp'type='submit'>Signin</button>
+            </form>
+        </div>
         )
     }
 }
