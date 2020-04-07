f478ce7814d4bb0ea05e5f3fbfe89c7f344f32e0
mckeekaylie <mckeekaylie@gmail.com>
2020-04-07 16:21:16 -0400

    start of home page

--- src/components/Petpage/Petinfo.tsx
+++ src/components/Petpage/Petinfo.tsx
@@ -1,31 +1,78 @@
 import React from 'react';
+import APIURL from '../../helpers/environment'
 
 // PROPS TYPE ALIAS
 type AcceptedProps = {
-    updateToken: (newToken: string) => void
+    token: any
 };
   
 // STATE TYPE ALIAS
-type SignupState = {
-    email: string,
-    password: string,
-    role: string
+type PetinfoState = {
+    pet: [],
+    modal: boolean
 };
 
-class Petinfo extends React.Component<AcceptedProps, SignupState> {
+class Petinfo extends React.Component<AcceptedProps, PetinfoState> {
     constructor(props: AcceptedProps){
         super(props);
         this.state = {
-          email: '',
-          password: '',
-          role: 'user'
+          pet: [],
+          modal: false
         }
-      }
-      
+    }
+
+    componentDidMount = () => {
+        const fetchPetinfo = () => {
+            const that = this;
+            fetch(`${APIURL}/petinfo/pet`, { 
+                method: 'GET',
+                headers: new Headers({
+                    'Content-Type': 'application/json',
+                    'Authorization': this.props.token
+                })
+            }) .then((res) => res.json())
+                .then((petData) => {
+                    that.setState({
+                        pet: petData
+                    })
+                    console.log(petData)
+                    console.log(that.state.pet)
+                })
+        }
+
+        fetchPetinfo();
+
+        const assignValues = () => {
+            // const petName = this.state.pet.map(pet => pet.name)
+        }
+
+        // const editPetinfo = (pet: any) => {
+        //     this.setState({
+        //         pet: pet
+        //     })
+        //     console.log(pet)
+        // }
+
+        // const modalOn = () => {
+        //     this.setState({
+        //         modal: true
+        //     })
+        // }
+    
+        // const modalOff = () => {
+        //     this.setState({
+        //         modal: false
+        //     })
+        // }
+    }
+
     render(){
         return(
             <div>
-                
+                <p></p>
+                <ul>
+                    {/* {this.state.pet.map(pet => pet.name)} */}
+                </ul>
             </div>
         )
     }
--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -2,7 +2,7 @@ let APIURL = '';
 
 switch (window.location.hostname) {
     case `localhost` || '127.0.0.1':
-        APIURL = 'http://localhost:3001';
+        APIURL = 'https://pawciousmemories-server.herokuapp.com';
         break;
     case 'pawciousmemories-server.herokuapp.com':
         APIURL = 'https://pawciousmemories-server.herokuapp.com'
