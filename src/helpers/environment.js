617acb57b6eb70bc5c2da56c3cf9c73d45289fec
mckeekaylie <mckeekaylie@gmail.com>
2020-04-15 09:26:50 -0400

    fixed environment

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -4,7 +4,7 @@ switch (window.location.hostname) {
     case `localhost` || '127.0.0.1':
         APIURL = 'https://pawciousmemories-server.herokuapp.com';
         break;
-    case 'pawciousmemories-server.herokuapp.com':
+    case 'pawciousmemories-client.herokuapp.com':
         APIURL = 'https://pawciousmemories-server.herokuapp.com'
 }
 
